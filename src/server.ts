6a5025db8bb7c342e4f8e3bd04024d0bd7b4b2a1
alexclemente1985 <alexclemente1985@gmail.com>
2022-03-20 20:44:33 -0300

    Criação de container docker com serviço nodejs

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(8080, () => {
-  console.log("server rodando em 8080");
+app.listen(3333, () => {
+  console.log("server rodando em 3333");
 });
